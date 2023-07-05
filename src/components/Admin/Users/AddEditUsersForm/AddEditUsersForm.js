4e5673ebad1a7e97523677cf25b42bbf42c9b955
Kevin Luna <kevin_2044@hotmail.com>
2023-07-05 00:39:03 -0600

    Se termino el CRUD de Users

---
+++ src/components/Admin/Users/AddEditUsersForm/AddEditUsersForm.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import { Form, Button, Checkbox } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { useUser } from '../../../../hooks'
+import './AddEditUsersForm.scss'
+
+export function AddEditUsersForm(props) {
+  const { onClose, onRefetch, user } = props
+  const { addUser, updateUser } = useUser()
+  const formik = useFormik({
+    initialValues: initialValues(user),
+    validationSchema: Yup.object(user ? updateSchema() : newSchema()),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        if(user){
+          console.log('editar usuario')
+          await updateUser(user.id, formValue)
+        }else{
+          await addUser(formValue)
+        }
+        onRefetch()
+        onClose()
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  })
+  return (
+    <Form className='add-edit-user-form' onSubmit={formik.handleSubmit}>
+        <Form.Input 
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
+          name="username" 
+          placeholder="Nombre de usuario" />
+        <Form.Input 
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+          name="email" 
+          placeholder="Correo Electronico" />
+        <Form.Input 
+          value={formik.values.first_name}
+          onChange={formik.handleChange}
+          error={formik.errors.first_name}
+          name="first_name" 
+          placeholder="Nombre" />
+        <Form.Input 
+          value={formik.values.last_name}
+          onChange={formik.handleChange}
+          error={formik.errors.last_name}
+          name="last_name" 
+          placeholder="Apellidos" />
+        <Form.Input
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+          name="password" 
+          type='password' 
+          placeholder="Contraseña" />
+        <div className='add-edit-user-form__active'>
+            <Checkbox toggle 
+              checked={formik.values.is_active} 
+              onChange={(_, data) => formik.setFieldValue('is_active', data.checked)} 
+            /> Usuario activo
+        </div>
+        <div className='add-edit-user-form__staff'>
+            <Checkbox toggle
+              checked={formik.values.is_staff} 
+              onChange={(_, data) => formik.setFieldValue('is_staff', data.checked)}
+            /> Usuario administrador
+        </div>
+        <Button type='submit' content={user ? "Actualizar" : "Crear"} primary fluid />
+    </Form>
+  )
+}
+
+function initialValues(user){
+  return {
+    username: user?.username || '',
+    email: user?.email ||'',
+    first_name: user?.first_name ||'',
+    last_name: user?.last_name ||'',
+    password: '',
+    is_active: user?.is_active ? true : false,
+    is_staff: user?.is_staff ? true : false,
+  }
+}
+
+function newSchema(){
+  return {
+      username: Yup.string().required(true),
+      email: Yup.string().email(true).required(true),
+      first_name: Yup.string(),
+      last_name: Yup.string(),
+      password: Yup.string().required(),
+      is_active: Yup.bool().required(true),
+      is_staff: Yup.bool().required(true),
+  }
+}
+
+function updateSchema(){
+  return {
+      username: Yup.string().required(true),
+      email: Yup.string().email(true).required(true),
+      first_name: Yup.string(),
+      last_name: Yup.string(),
+      password: Yup.string(),
+      is_active: Yup.bool().required(true),
+      is_staff: Yup.bool().required(true),
+  }
+}
--- src/pages/Admin/UserAdmin.js
+++ src/pages/Admin/UserAdmin.js
@@ -1,40 +1,86 @@
 import React, { useState, useEffect } from 'react'
 import { Loader } from 'semantic-ui-react';
 import { useUser } from '../../hooks'
-import { HeaPage, TableUsers } from '../../components/Admin';
+import { AddEditUsersForm, HeaPage, TableUsers } from '../../components/Admin';
 import { ModalBasic } from '../../components/Common'
 
 /* import { useAuth } from '../../hooks' */
 
 export function UserAdmin() {
     const [showModal, setShowModal] = useState(false)
+    // eslint-disable-next-line
     const [titleModal, setTitleModal] = useState(null)
     const [contentModal, setContentModal] = useState(null)
-    const { loading, error, users, getUsers} = useUser();
-    console.log('loading',loading)
-    console.log('users',users)
+    const [refetch, setRefetch] = useState(false)
+    // eslint-disable-next-line
+    const { loading, error, users, getUsers, deleteUser} = useUser();
+    /* console.log('loading',loading)
+    console.log('users',users) */
     useEffect(() => {
         getUsers()
-    }, [])
+        // eslint-disable-next-line
+    }, [refetch])
 
     const openCloseModal = () => setShowModal((prev) => {
         return !prev
     })
+
+    const onRefetch = () => {
+        return setRefetch((prev) => !prev)
+    }
+
+    const addUser = () => {
+        setTitleModal("Nuevo usuario")
+        setContentModal(
+            <AddEditUsersForm 
+                onClose={openCloseModal} 
+                onRefetch={onRefetch} 
+            />
+        )
+        openCloseModal()
+    }
+
+    const updateUser = (data) => {
+        console.log('Editar')
+        console.log(data)
+        setTitleModal("Actualizar usuario")
+        setContentModal(
+            <AddEditUsersForm 
+                onClose={openCloseModal} 
+                onRefetch={onRefetch}
+                user={data} 
+            />
+        )
+        openCloseModal()
+    }
+
+    const onDeleteUser = async(data) => {
+        const result = window.confirm(`Eliminar usuario ${data.email}`)
+        if(result){
+            try {
+                await deleteUser(data.id)
+                onRefetch()
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
     
     return (
         <div>
-            <HeaPage title='Usuarios' btnTitle='Nuevo Usuario' btnClick={openCloseModal} />
+            <HeaPage title='Usuarios' btnTitle='Nuevo Usuario' btnClick={addUser} />
             {loading ? (
                 <Loader active inline="centered">
                     Cargado...
                 </Loader>
             ): (
-                <TableUsers users={users} />
+                <TableUsers users={users} updateUser={updateUser} onDeleteUser={onDeleteUser} />
             )}
             <ModalBasic 
-                show={true} 
+                show={showModal} 
+                onClose={openCloseModal}
                 title='Crear usuario' 
-                children={<h2>Children</h2>} 
+                children={contentModal} 
             />
         </div>
     )
