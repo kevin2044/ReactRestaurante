caf29d2ca7ece2e8941bad06086d39424296e4e4
Kevin Luna <kevin_2044@hotmail.com>
2023-07-17 01:29:32 -0600

    Agregando endpoints y hook de products

--- src/api/product.js
+++ src/api/product.js
@@ -1,1 +1,83 @@
-export { BASE_API } from '../utils/constants'
+import { BASE_API } from '../utils/constants'
+
+export async function getProductsApi(token) {
+    try {
+        const url = `${BASE_API}api/products/`
+
+        const response = await fetch(url)
+        const result = await response.json()
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function addProductApi(token, data){
+    try {
+        const formData = new FormData()
+        formData.append('image', data.image)
+        formData.append('title', data.title)
+        formData.append('price', data.price)
+        formData.append('active', data.active)
+        formData.append('category', data.category)
+
+        const url = `${BASE_API}api/products/`
+        const params = {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            body: formData,
+        }
+        const response = await fetch(url, params)
+        const result = await response.json()
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function updateProductApi(id, data, token){
+    try {
+        const formData = new FormData()
+        if(data.image){
+            formData.append('image', data.image)
+        }
+        formData.append('title', data.title)
+        formData.append('price', data.price)
+        formData.append('active', data.active)
+        formData.append('category', data.category)
+
+        const url = `${BASE_API}api/products/`
+        const params = {
+            method: "PATCH",
+            headers: {
+                Authorizations: `Bearer ${token}`,
+            },
+            body: formData,
+        }
+        const response = await fetch(url, params)
+        const result = await response.json()
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function deleteProductApi(id, token){
+    try {
+        const url = `${BASE_API}api/products/`
+        const params = {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+
+        const response = await fetch(url, params)
+        const result = await response.json()
+        return result
+    } catch (error) {
+        throw error
+    }
+}
--- src/pages/Admin/ProductsAdmin.js
+++ src/pages/Admin/ProductsAdmin.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { HeaPage } from '../../components/Admin'
+import { Loader } from 'semantic-ui-react'
 
 export function ProductsAdmin() {
-  return (
-    <div>ProductsAdmin</div>
-  )
+    return (
+        <div>
+        <HeaPage title='Productos' btnTitle='Nuevo Producto'  />
+        { loading ? (
+            <Loader active inline="centered">
+                Cargando...
+            </Loader>
+        ): (
+            <TableProductAdmin />
+        )}
+        </div>
+    )
 }
